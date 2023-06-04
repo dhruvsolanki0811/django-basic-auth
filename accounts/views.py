1ec7e0fa3bf5392cba4c7f23a2b7208cb4b943c0
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2023-06-04 12:59:07 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserForm
+# Create your views here.
+def register(request):
+    if request.method=='POST':
+        form=UserForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get('username')
+            form.save()
+            messages.success(request,f'form submitted with name {username}')
+            return redirect('accounts:create_user')
+    else:
+        form=UserForm()
+    return render(request,'accounts/register.html',{'form':form})
+
+def home(request):
+    return render(request,'accounts/profile.html',{})
